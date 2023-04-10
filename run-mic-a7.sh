aa2d8f609334c74eab22260190a84bb1eaaf53a8
aymanrgab <aymanrar2c@gmail.com>
2023-04-10 19:45:25 +0200

    add a7

---
+++ run-mic-a7.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+source /a7y17lte/a7y17lte.env
+
+sudo zypper in -y kmod
+sudo zypper -n in lvm2
+
+echo "Creating loop devices..."
+sudo mknod /dev/loop0 -m0660 b 7 0
+sudo mknod /dev/loop1 -m0660 b 7 1
+sudo mknod /dev/loop2 -m0660 b 7 2
+echo $?
+ls -lh /dev/loop*
+
+echo "Building Sailfish $RELEASE"
+
+sudo zypper in -y kmod 
+sudo mic create fs --arch=$PORT_ARCH \
+--tokenmap=ARCH:$PORT_ARCH,RELEASE:$RELEASE,EXTRA_NAME:$EXTRA_NAME \
+--record-pkgs=name,url \
+--outdir=sfe-$DEVICE-$RELEASE$EXTRA_NAME \
+--pack-to=sfe-$DEVICE-$RELEASE$EXTRA_NAME.tar.bz2 \
+Jolla-@RELEASE@-$DEVICE-@ARCH@.ks
